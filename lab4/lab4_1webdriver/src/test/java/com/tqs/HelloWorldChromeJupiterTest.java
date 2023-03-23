7be17f3344d4d7ce33408a717d7f66090cbae2bb
Tiago Sora <tiagogcarvalho2002@gmail.com>
2023-03-23 16:09:14 +0000

    setting up lab5

--- lab4/lab4_1webdriver/src/test/java/com/tqs/HelloWorldChromeJupiterTest.java
+++ lab4/lab4_1webdriver/src/test/java/com/tqs/HelloWorldChromeJupiterTest.java
@@ -8,12 +8,15 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.slf4j.Logger;
 
+import io.github.bonigarcia.seljup.SeleniumJupiter;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
+@ExtendWith(SeleniumJupiter.class)
 public class HelloWorldChromeJupiterTest {
 
     static final Logger log = getLogger(lookup().lookupClass());
