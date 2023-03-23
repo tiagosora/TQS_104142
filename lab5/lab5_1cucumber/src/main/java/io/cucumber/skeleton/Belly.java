9155c078f945fb0f619b12b40cb3895a6824a8a8
Tiago Sora <tiagogcarvalho2002@gmail.com>
2023-03-23 17:45:59 +0000

    lab5_1 done

--- lab5/lab5_1cucumber/src/main/java/io/cucumber/skeleton/Belly.java
+++ lab5/lab5_1cucumber/src/main/java/io/cucumber/skeleton/Belly.java
@@ -1,7 +1,25 @@
 package io.cucumber.skeleton;
 
 public class Belly {
-    public void eat(int cukes) {
+    private int cukesInBelly;
 
+    public Belly() {
+        this.cukesInBelly = 0;
+    } 
+
+    public void eat(int cukes) {
+        this.cukesInBelly = cukes;
+        System.out.println("I have " + this.cukesInBelly + " cukes in my belly");
+    }
+    public void wait(int hours) {
+        this.cukesInBelly = 0;
+        System.out.println("I wait " + hours + " hour");
+    }
+
+    public int getCukesInBelly() {
+        return cukesInBelly;
+    }
+    public void setCukesInBelly(int cukesInBelly) {
+        this.cukesInBelly = cukesInBelly;
     }
 }
--- lab5/lab5_1cucumber/src/test/java/io/cucumber/skeleton/RunCucumberTest.java
+++ lab5/lab5_1cucumber/src/test/java/io/cucumber/skeleton/RunCucumberTest.java
@@ -1,12 +1,12 @@
 package io.cucumber.skeleton;
 
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
-
-import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
@@ -14,4 +14,6 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "io.cucumber.skeleton")
 public class RunCucumberTest {
+
 }
+    
